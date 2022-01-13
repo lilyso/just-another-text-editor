00fc3048906aea388dc9f21169bd1322d330c238
lilys0 <lsouryaphanh@gmail.com>
2022-01-13 16:20:16 +1100

    database error fix

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -16,16 +16,16 @@ const initdb = async () =>
 export const putDb = async (content) => {
   console.log("Post to the database");
   // Create a connection to the database database and version we want to use.
-  const contactDb = await openDB("contact", 1);
+  const jateDb = await openDB("jate", 1);
 
   // Create a new transaction and specify the database and data privileges.
-  const tx = contactDb.transaction("contact", "readwrite");
+  const tx = jateDb.transaction("jate", "readwrite");
 
   // Open up the desired object store.
-  const store = tx.objectStore("contact");
+  const store = tx.objectStore("jate");
 
   // Use the .add() method on the store and pass in the content.
-  const request = store.add(content);
+  const request = store.put({ content: content, id: 1 });
 
   // Get confirmation of the request.
   const result = await request;
@@ -37,13 +37,13 @@ export const getDb = async () => {
   console.log("GET from the database");
 
   // Create a connection to the database database and version we want to use.
-  const contactDb = await openDB("contact", 1);
+  const jateDb = await openDB("jate", 1);
 
   // Create a new transaction and specify the database and data privileges.
-  const tx = contactDb.transaction("contact", "readonly");
+  const tx = jateDb.transaction("jate", "readonly");
 
   // Open up the desired object store.
-  const store = tx.objectStore("contact");
+  const store = tx.objectStore("jate");
 
   // Use the .getAll() method to get all data in the database.
   const request = store.getAll();
